bcd641272211dba536489ad19c0ea2f6c7c2e9f7
Leydi-Aro <leydiaro4@gmail.com>
2024-10-31 16:05:38 -0500

    Implementing controllers
    feat: Implementing controllers

--- src/main/java/com/aharon/sensors/controller/SensorController.java
+++ src/main/java/com/aharon/sensors/controller/SensorController.java
@@ -1,4 +1,36 @@
 package com.aharon.sensors.controller;
 
+import com.aharon.common.dto.ApiResponse;
+import com.aharon.sensors.dto.CreateSensor;
+import com.aharon.sensors.dto.SensorResponse;
+import com.aharon.sensors.service.SensorService;
+import com.aharon.zones.dto.CreateZone;
+import com.aharon.zones.dto.ZoneResponse;
+import com.aharon.zones.service.ZoneService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sensors")
+@AllArgsConstructor
 public class SensorController {
+    private final SensorService sensorService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<SensorResponse>> addSensor(@Valid @RequestBody CreateSensor createSensor) {
+        SensorResponse sensorResponse = sensorService.addSensor(createSensor);
+
+        ApiResponse<SensorResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setSuccess(true);
+        apiResponse.setMessage("Sensor created successfully.");
+        apiResponse.setData(sensorResponse);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
 }
