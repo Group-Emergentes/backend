ce5b8d139ac8341db5f9d859c2c4810d7b8a1016
edwinlopezz17 <u20201b051@upc.edu.pe>
2024-10-30 23:15:46 -0500

    firts commit

---
+++ aharon/src/main/java/com/aharon/aharon/AharonApplication.java
@@ -0,0 +1,13 @@
+package com.aharon.aharon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AharonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AharonApplication.class, args);
+	}
+
+}
---
+++ aharon/src/test/java/com/aharon/aharon/AharonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aharon.aharon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AharonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
