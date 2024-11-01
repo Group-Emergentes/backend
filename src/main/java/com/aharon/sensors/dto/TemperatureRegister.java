3411c9e569ef3cd6cbb50a67747f9cfba6f1efdc
edwinlopezz17 <u20201b051@upc.edu.pe>
2024-10-31 19:13:11 -0500

    temperature history table complete

---
+++ src/main/java/com/aharon/sensors/dto/TemperatureRegister.java
@@ -0,0 +1,16 @@
+package com.aharon.sensors.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class TemperatureRegister {
+
+    @NotNull
+    private String sensorId;
+    @NotNull
+    private Double value;
+    @NotNull
+    private Long zoneId;
+
+}
